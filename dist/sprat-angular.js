1796b3cb9b7b27760c0b46219f773dd3615e000b
Stefan Fiedler <stefan.fiedler@neos-it.de>
2016-03-03 11:40:14 +0100

    * changed return value of ui.batchRemove method * now returns the amount of deleted users instead of boolean

--- dist/sprat-angular.js
+++ dist/sprat-angular.js
@@ -372,7 +372,7 @@ sprat.ui.batchRemove = function($items, $service, $opts) {
 	
 	// ask for confirmation
 	if (toRemove.length <= 0 || !opts.confirm(toRemove)) {
-		return false;
+		return 0;
 	}
 
 	for (var i = 0, m = toRemove.length; i < m; i++) {
@@ -384,7 +384,7 @@ sprat.ui.batchRemove = function($items, $service, $opts) {
 		});
 	}
 	
-	return true;
+	return toRemove.length;
 };
 
 var sprat = sprat || {};
